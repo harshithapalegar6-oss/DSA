1f95bb9c001bc56f9fe439ff9e862331d85a39c9
harshithapalegar6-oss <harshithapalegar6@gmail.com>
2026-06-26 21:07:38 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (17.12%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+          if (x < 0) {
+            return false;
+        }
+        int num=x;
+        int original=num;
+        int rev=0;
+        while(num!=0){
+            int digit=num%10;
+            rev=rev*10+digit;
+            num=num/10;
+        }
+        return original==rev;
+    }
+}
