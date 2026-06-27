5f37d35dce9d230395119f20d1bed1f79250eea3
harshithapalegar6-oss <harshithapalegar6@gmail.com>
2026-06-27 22:05:06 +0530

    Time: 667 ms (12.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 178-rank-scores/rank-scores.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    s1.score,
+    COUNT(DISTINCT s2.score) + 1 AS `rank`
+FROM Scores s1
+LEFT JOIN Scores s2
+ON s2.score > s1.score
+GROUP BY s1.id, s1.score
+ORDER BY s1.score DESC;
